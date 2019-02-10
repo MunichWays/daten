c2574fca8fdd6b98f47a91c2b91ed5433f7a293b
Jan Langfellner <jan.langfellner@web.de>
2019-02-10 15:16:57 +0100

    Change KML layer extraction script to support Python3

--- process_master.py
+++ process_master.py
@@ -1,33 +1,31 @@
-import sys
-from fastkml import kml
-
-# reconfigure default encoding to utf8 for python2
-if sys.version_info.major < 3:
-    reload(sys)
-    sys.setdefaultencoding('utf-8')
-
+import fastkml
 
 # fastkml usage examples https://github.com/cleder/fastkml/tree/master/examples
 
-k = kml.KML()
-with open("RadlVorrangnetz-Master.kml", "r") as fp:
-    doc = fp.read()
+k = fastkml.kml.KML()
+
+# Read in RadlVorrangnetz KML file from Google MyMaps
+with open("RadlVorrangnetz-Master.kml", "r", encoding="utf-8") as fp:
+    doc = fp.read().encode('utf-8')
     k.from_string(doc)
 
+# Extract features from KML
 features = list(k.features())
+layers = list(features[0].features())  # Layers are KML folder objects
 
-for folder in list(features[0].features()):
-    print(folder.name)
+# Loop over the layers
+for layer in layers:
+    print(f'Processing layer {layer.name}...')
 
     # Create the root KML object
-    k = kml.KML()
+    k = fastkml.kml.KML()
     ns = '{http://www.opengis.net/kml/2.2}'
 
     # Create a KML Document and add it to the KML root object
-    d = kml.Document(ns, 'docid', 'doc name', 'doc description')
+    d = fastkml.kml.Document(ns, 'docid', 'doc name', 'doc description')
     k.append(d)
+    d.append(layer)
 
-    d.append(folder)
-
-    with open(u"{}.kml".format(folder.name), "w") as fp:
+    # Write each layer to separate KML file
+    with open(u"{}.kml".format(layer.name), "w") as fp:
         fp.write(k.to_string(prettyprint=True))

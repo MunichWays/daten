73925b153196a3c28ce1a3939fe803ce314dfed7
Jan Langfellner <jan.langfellner@web.de>
2019-02-20 21:40:43 +0100

    Add parsing of color prefixes in placemarks: E.g., lines with "gelb_" get a yellow color tag added to their line style

--- build_kmls.py
+++ build_kmls.py
@@ -2,4 +2,5 @@ import munichways_data.kmltranslate
 
 translator = munichways_data.kmltranslate.KMLTranslator()
 translator.extract_layers()
+translator.color_placemarks()
 translator.write_split_layers()
--- src/munichways_data/kmltranslate.py
+++ src/munichways_data/kmltranslate.py
@@ -1,6 +1,5 @@
 import fastkml
-
-# fastkml usage examples https://github.com/cleder/fastkml/tree/master/examples
+import re
 
 
 class KMLTranslator:
@@ -15,6 +14,7 @@ class KMLTranslator:
         self.k = fastkml.kml.KML()
         self.features = None
         self.layers = None
+        self.styles = self._define_styles()
 
         # Read in RadlVorrangnetz KML file from Google MyMaps
         print(f'Reading in file {self.input_file}')
@@ -28,6 +28,51 @@ class KMLTranslator:
         self.layers = list(self.features[0].features())  # Layers are KML folder objects
         print(f'Extracted {len(self.layers)} layers from input file')
 
+    @staticmethod
+    def _define_styles():
+        """Define line styles that represent the cycling conditions"""
+        yellow_line = fastkml.Style(id='gelb', styles=[fastkml.LineStyle(color='ffff00', width=2)])
+        red_line = fastkml.Style(id='rot', styles=[fastkml.LineStyle(color='ff0000', width=2)])
+        green_line = fastkml.Style(id='grün', styles=[fastkml.LineStyle(color='00ff00', width=2)])
+        black_line = fastkml.Style(id='schwarz', styles=[fastkml.LineStyle(color='000000', width=2)])
+        grey_line = fastkml.Style(id='grau', styles=[fastkml.LineStyle(color='888888', width=2)])
+        return [green_line, yellow_line, red_line, black_line, grey_line]
+
+    def color_placemarks(self):
+        """
+        Add color line style to each placemark based on its name:
+        gelb_ -> yellow_line
+        rot_ -> red_line
+        grün_ -> green_line
+        schwarz_ -> black_line
+        anything else -> grey_line
+        """
+        processed_layers = []
+        for layer in self.layers:
+            processed_layer = fastkml.Folder(ns=layer.ns,
+                                             id=layer.id,
+                                             name=layer.name,
+                                             description=layer.description)
+            placemarks = list(layer.features())
+            for placemark in placemarks:
+                processed_placemark = self._set_styleurl_by_color_prefix(placemark)
+                processed_layer.append(processed_placemark)
+            processed_layers.append(processed_layer)
+
+        self.layers = processed_layers
+
+    @staticmethod
+    def _set_styleurl_by_color_prefix(placemark):
+        """Sets style url of placemark depending on its color prefix"""
+        color_pattern = re.compile('^(\w+)_.*')
+        match = re.match(color_pattern, placemark.name)
+        if match:
+            color = match[1]
+        else:
+            color = 'grau'
+        placemark.styleUrl = color
+        return placemark
+
     def write_split_layers(self):
         """Write each single layer to separate KML file"""
         # Loop over the layers
@@ -39,7 +84,11 @@ class KMLTranslator:
             ns = '{http://www.opengis.net/kml/2.2}'
 
             # Create a KML document and add it to the KML root object
-            doc = fastkml.kml.Document(ns, 'docid', 'doc name', 'doc description')
+            doc = fastkml.kml.Document(ns=ns,
+                                       id='docid',
+                                       name='doc name',
+                                       description='doc description',
+                                       styles=self.styles)
             k_out.append(doc)
 
             # Add layer to KML document
@@ -48,9 +97,3 @@ class KMLTranslator:
             # Write KML object for this layer to KML file
             with open(u"{}{}.kml".format(self.output_dir, layer.name), "w") as fp:
                 fp.write(k_out.to_string(prettyprint=True))
-
-
-if __name__ == '__main__':
-    bla = KMLTranslator()
-    bla.extract_layers()
-    bla.write_split_layers()
